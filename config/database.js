21d4c22e86dc9eb5182e90c1365e88ca4c5d9521
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2022-02-23 01:55:06 +0530

    story : editorjs submission json schema

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'reading-room'),
       user: env('DATABASE_USERNAME', 'reading-room'),
-      password: env('DATABASE_PASSWORD', 'reading-room'),
+      password: env('DATABASE_PASSWORD', 'reading'),
       // ssl: env.bool('DATABASE_SSL', true),
     },
   },
