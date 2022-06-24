ebde32ae6ae3e0ec4802e3217539a4367f5f3934
Anushka Trivedi <anushka@mimaze.co.jp>
2022-06-24 14:54:56 +0900

    removing cloudinary uploads temp removing visited field from user

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,18 +1,4 @@
 module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET,
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-  },
   email: {
     config: {
       provider: 'nodemailer',
