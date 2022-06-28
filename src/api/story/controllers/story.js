b9a3c45d758c577bfb8ccfbded80cc5a7c205bc2
Anushka Trivedi <anushka@mimaze.co.jp>
2022-06-28 16:38:16 +0900

    story entity - new route /random

--- src/api/story/controllers/story.js
+++ src/api/story/controllers/story.js
@@ -6,4 +6,10 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::story.story');
+module.exports = createCoreController('api::story.story', ({ strapi }) => ({
+  
+  async random(ctx) {
+    return strapi.service('api::story.story').random()
+  }
+
+}));
--- src/api/story/services/story.js
+++ src/api/story/services/story.js
@@ -6,4 +6,29 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::story.story');
+module.exports = createCoreService('api::story.story', ({ strapi }) => ({
+
+  // find a random story
+  async random(...args) {
+
+    try {
+      const knex = strapi.db.connection
+      
+      const record = await knex
+      .select('id', 'title', 'location', 'description', 'submission', 'published_at as publishedAt')
+      .from('stories')
+      .orderByRaw('random()')
+      .limit(1)
+
+      console.log()
+
+      console.log(`random() record: ${record[0].id}`)
+
+      return record[0]
+    } catch(err) {
+      return null
+    }
+
+  }
+
+}));
