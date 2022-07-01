a1688f46c639cd857578f3f6efd5a9f9dd2065b4
rndexe <himanshu.ernde@gmail.com>
2022-07-02 00:11:00 +0530

    add hasDraft and users/stories route

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,11 +1,47 @@
 module.exports = plugin => {
     const sanitizeOutput = (user) => {
         const {
-          password, resetPasswordToken, confirmationToken, ...sanitizedUser
+            password, resetPasswordToken, confirmationToken,
+            provider, confirmed, blocked, createdAt,
+            updatedAt, email, ...sanitizedUser
         } = user; // be careful, you need to omit other private attributes yourself
         return sanitizedUser;
-        
     };
+
+    const stories = async (ctx) => {
+
+        console.log("have I hit the action")
+        if (!ctx.state.user) {
+            return ctx.unauthorized();
+        }
+
+        const user = await strapi.entityService.findOne(
+            'plugin::users-permissions.user',
+            ctx.state.user.id,
+            {
+                populate: {
+                    stories: {
+                        fields: ['id', 'slug', 'title', 'location', 'description', 'publishedAt', 'hasDraft']
+                    },
+                },
+            }
+        );
+        ctx.body = sanitizeOutput(user);
+        //return;
+        //console.log(ctx.state.user)
+    }
+
+    const userStoriesRoute = {
+        method: 'GET',
+        path: '/users/stories',
+        handler: 'user.stories',
+        config: { prefix: '' }
+    }
+
+    plugin.routes['content-api'].routes.splice(10, 0, userStoriesRoute);
+    plugin.controllers.user['stories'] = stories;
+
+    console.log(plugin.controllers)
 
     plugin.controllers.user.me = async (ctx) => {
         if (!ctx.state.user) {
@@ -14,13 +50,12 @@ module.exports = plugin => {
         const user = await strapi.entityService.findOne(
             'plugin::users-permissions.user',
             ctx.state.user.id,
-            { 
+            {
                 populate: {
                     stories: {
-                        fields: ['id','slug','title','location','description','publishedAt']
+                        fields: ['id', 'slug', 'title', 'location', 'description', 'publishedAt']
                     },
                     maps: {
-                        
                         filters: {
                             title: {
                                 $eq: null,
@@ -31,12 +66,9 @@ module.exports = plugin => {
                         }
                     }
                 },
-                
-            }   
+            }
         );
-
         ctx.body = sanitizeOutput(user);
     };
-
     return plugin;
 };
